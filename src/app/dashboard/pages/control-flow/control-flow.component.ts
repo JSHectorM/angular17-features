a1c85bfb9ea45ddcdd8d4affc473206d86d7d50e
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-11-22 20:28:55 -0600

    Add signal con if else

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +10,12 @@ import { RouterOutlet } from '@angular/router';
   styles: ``
 })
 export default class ControlFlowComponent {
+  
+  public showContent = signal(false);
+
+
+  public toggleContent() {
+    this.showContent.update( value => !value)
+  }
 
 }
