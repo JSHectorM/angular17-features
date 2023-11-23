68d2c53d7494cc86bc822aed72967598967c10d3
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-11-22 21:24:09 -0600

    Add control switch y for con sus propiedades

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -2,6 +2,8 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+type Grade = "A" | "B" | "C" | "D" | "F";
+
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -12,10 +14,24 @@ import { RouterOutlet } from '@angular/router';
 export default class ControlFlowComponent {
   
   public showContent = signal(false);
+  
+  public grade = signal<Grade>("A");
 
+  public grades = signal<Grade[]>(["A","B","C","D","F"]);
+
+  public frameworks = signal(["Angular","React","Vue", "Svelte", "Ember", "Quik"  ]);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
-    this.showContent.update( value => !value)
+    this.showContent.update(value => !value);
+  }
+
+  public toggleGrade($event: Event) {
+    const target = $event?.target as HTMLInputElement;
+    if (target) {
+      console.log(target.value);
+      this.grade.update( () => target.value as Grade );
+    }
   }
 
 }
